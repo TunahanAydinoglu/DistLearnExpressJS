615ebc63148f97bc9b18539c3bd2ef6e2fdeaebf
TunahanAydinoglu <tunahanaydinoglu@outlook.com>
2021-08-07 15:17:18 +0300

    Update index.js

--- routes/a/index.js
+++ routes/a/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const restaurant = require("./restaurant");
+const restaurant = require("./meal");
 const router = express.Router();
 
 router.use(cors());
 router.use("/restaurant", restaurant);
+router.use("/meal", meal);
 
 module.exports = router;
