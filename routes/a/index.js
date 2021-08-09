9eec43c2ad51fec7c0bb0c192083cdcf20cff366
TunahanAydinoglu <tunahanaydinoglu@outlook.com>
2021-08-09 19:25:20 +0300

    try fix for deploy

--- routes/a/index.js
+++ routes/a/index.js
@@ -3,15 +3,11 @@ const cors = require("cors");
 
 const restaurant = require("./restaurant");
 const meal = require("./meal");
-const { addNewOrder } = require("../../controllers/a/Order");
-const { getAccessToRoute } = require("../../middlewares/authorization/auth");
-const { checkOrderAvailable } = require("../../middlewares/database/databaseErrorHelpers");
 const router = express.Router();
 
 router.use(cors());
 router.use("/restaurant", restaurant);
 router.use("/meal", meal);
 
-// router.post("/order",[getAccessToRoute,checkOrderAvailable], addNewOrder);
 
 module.exports = router;
